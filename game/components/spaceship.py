d9a93aae1967c142b516bad81b17a9d6d20d3a2d
dennis-adrian <dennisguzmanbo@gmail.com>
2023-07-18 11:08:23 -0300

    HW solution with methods

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -16,10 +16,10 @@ class Spaceship(Sprite):
         self.rect = self.image.get_rect(midbottom = (self.SPACESHIP_POS_X, self.SPACESHIP_POS_Y))
 
     def update(self, user_input):
-        if user_input[pygame.K_LEFT] and self.rect.left > 0:
-            self.rect.x -= 10
-        elif user_input[pygame.K_RIGHT] and self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10
+        if user_input[pygame.K_LEFT]:
+            self.move_left()
+        elif user_input[pygame.K_RIGHT]:
+            self.move_right()
         elif user_input[pygame.K_UP] and self.rect.top > 300:
             self.rect.y -= 10
         elif user_input[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
@@ -27,3 +27,14 @@ class Spaceship(Sprite):
     
     def draw(self, screen):
         screen.blit(self.image, self.rect)
+
+    def move_right(self):
+        self.rect.x += 10
+        if self.rect.right >= SCREEN_WIDTH:
+            self.rect.left = -self.SPACESHIP_WIDTH
+    
+    def move_left(self):
+        self.rect.x -= 10
+        if self.rect.left <= 0:
+            self.rect.right = SCREEN_WIDTH + self.SPACESHIP_WIDTH
+
