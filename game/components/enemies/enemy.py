26bc938d6ed6908ab9a9588883823c675eecbab9
dennis-adrian <dennisguzmanbo@gmail.com>
2023-07-19 15:02:16 -0300

    Class 2 HW

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,32 +2,32 @@ import pygame
 import random
 from pygame.sprite import Sprite
 
-from game.utils.constants import ENEMY_1, SCREEN_HEIGHT, SCREEN_WIDTH
+from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
     Y_POS = 0
     X_POS_RANGE = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
-    SPEED_ON_Y = 1
-    SPEED_ON_X = 10
     MOVES = { 0: 'left', 1: 'right' }
 
-    def __init__(self):
-        self.image = ENEMY_1
+    def __init__(self, image, speed_on_x, speed_on_y):
+        self.image = image
         self.image = pygame.transform.scale(self.image, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
         self.rect = self.image.get_rect(midtop = (random.choice(self.X_POS_RANGE), self.Y_POS))
         self.direction = self.MOVES[random.randint(0, 1)]
         self.movement_count = 0
         self.moves_before_change = random.randint(20, 50)
+        self.speed_on_x = speed_on_x
+        self.speed_on_y = speed_on_y
 
     def update(self, enemies):
-        self.rect.y += self.SPEED_ON_Y
+        self.rect.y += self.speed_on_y
 
         if self.direction == self.MOVES[0]:
-            self.rect.x -= self.SPEED_ON_X
+            self.rect.x -= self.speed_on_x
         elif self.direction == self.MOVES[1]:
-            self.rect.x += self.SPEED_ON_X
+            self.rect.x += self.speed_on_x
 
         self.handle_direction()
 
--- game/components/enemies/enemy_manager.py
+++ game/components/enemies/enemy_manager.py
@@ -1,9 +1,12 @@
-import pygame
+import random
+
 from game.components.enemies.enemy import Enemy
+from game.utils.constants import ENEMY_1, ENEMY_2, ENEMY_3
 
 class EnemyManager:
     def __init__(self):
         self.enemies = []
+        self.enemy_images = [ENEMY_1, ENEMY_2, ENEMY_3]
     
     def update(self):
         self.add_enemy()
@@ -16,6 +19,10 @@ class EnemyManager:
             enemy.draw(screen)
 
     def add_enemy(self):
-        if len(self.enemies) < 1:
-            enemy = Enemy()
+        if len(self.enemies) < 2:
+            image = random.choice(self.enemy_images)
+            speed_on_x = random.randint(10, 20)
+            speed_on_y = random.randint(1, 5)
+
+            enemy = Enemy(image, speed_on_x, speed_on_y)
             self.enemies.append(enemy)
